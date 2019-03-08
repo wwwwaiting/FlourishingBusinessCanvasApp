fccd94f1cf4e3a61508da6dd0b41d9c987504e15
Luya Wang <luya.wang2@canada.ca>
2019-03-08 13:18:11 -0500

    add functionality for sending email after user registration

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(express.static('public'));
@@ -43,35 +45,37 @@ const registrationRquest = new Array();
 
 app.get('/login', function(req, res) {
   res.render('login');
-})
+});
 
 app.post('/login', function(req, res) {
   var email = req.body.email;
   var pwd = req.body.pwd;
   var name;
-  User.find({
-    'email': email,
-    'status': 2
-  }, function(err, result) {
-    if (err) {
-      console.log(err);
-      res.send(fal);
-    } else if (result.length === 0) {
-      console.log('user does not exist or does not have the authorization to login!');
-      res.send(denied);
-    } else {
-      var user = result[0];
-      name = user.name;
-      if (pwd === user.pwd) {
-        res.cookie('name', name);
-        res.send(tru);
-      } else {
+  User.find(
+    {
+      email: email,
+      status: 2,
+    },
+    function(err, result) {
+      if (err) {
+        console.log(err);
         res.send(fal);
+      } else if (result.length === 0) {
+        console.log('user does not exist or does not have the authorization to login!');
+        res.send(denied);
+      } else {
+        var user = result[0];
+        name = user.name;
+        if (pwd === user.pwd) {
+          res.cookie('name', name);
+          res.send(tru);
+        } else {
+          res.send(fal);
+        }
       }
     }
-  })
-
-})
+  );
+});
 
 // register for new user
 app.post('/register', function(req, res) {
@@ -79,53 +83,59 @@ app.post('/register', function(req, res) {
   var pwd = req.body.pwd;
   var email = req.body.email;
   var canvasList = new Array();
-  User.findOneAndUpdate({
-    'email': email,
-    'pwd':'',
-    'status': 2
-  }, {
-    pwd: pwd
-  }, function(err, result) {
-    if (err) {
-      console.log(err);
-      res.send(fal);
-    } else if (result === null) {
-      User.find({
-        'email': email
-      }, function(err, result) {
-        if (err) {
-          console.log(err);
-          res.send(fal);
-        } else if (result.length !== 0) {
-          console.log('The email has been registered in the system!');
-          res.send(fal);
-        } else {
-          registrationRquest.push(email);
-          var user = new User({
-            name: name,
+  User.findOneAndUpdate(
+    {
+      email: email,
+      pwd: '',
+      status: 2,
+    },
+    {
+      pwd: pwd,
+    },
+    function(err, result) {
+      if (err) {
+        console.log(err);
+        res.send(fal);
+      } else if (result === null) {
+        User.find(
+          {
             email: email,
-            pwd: pwd,
-            role: 1,
-            canvas: canvasList,
-            occupation: '',
-            status: 1
-          });
-          User.create(user, function(err, newlyCreated) {
+          },
+          function(err, result) {
             if (err) {
               console.log(err);
+              res.send(fal);
+            } else if (result.length !== 0) {
+              console.log('The email has been registered in the system!');
+              res.send(fal);
             } else {
-              res.cookie('name', name);
-              res.send(tru);
+              registrationRquest.push(email);
+              var user = new User({
+                name: name,
+                email: email,
+                pwd: pwd,
+                role: 1,
+                canvas: canvasList,
+                occupation: '',
+                status: 1,
+              });
+              User.create(user, function(err, newlyCreated) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  res.cookie('name', name);
+                  res.send(tru);
+                }
+              });
             }
-          });
-        };
-      })
+          }
+        );
+      } else {
+        res.send(tru);
+      }
     }
-    else {
-      res.send(tru);
-    }
-  });
-})
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
