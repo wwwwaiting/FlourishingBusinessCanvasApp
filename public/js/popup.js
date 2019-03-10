ca592171678702f5c7773f6aa3543c16b40b3f94
jhma17 <jma910212@gmail.com>
2019-03-10 00:02:25 -0500

    Dynamic change

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,91 +1,62 @@
 "use strict"
 console.log("popup.js")
-
-class User{
-    constructor(username){
-        this.username = username;
-    }
-}
-
+let canvasUsers = null
+let canvasIds = null
+let canvasTitles = null
 class Fbc{
-    constructor(users, name){
+    constructor(users, name, id){
         this.users = users;
-        this.name = name
+        this.name = name;
+        this.id = id;
+    }
+}
+const fbcs = []
+for (let i = 1; i <= 7; i++) {
+    const users = []
+    for (let e = 1; e <= i; e++) {
+        users.push(e)
+    }
+    fbcs.push(new Fbc(users, `fbc${i}`, i))
+}
+console.log(fbcs)
+
+
+$(document).ready(main);
+
+
+// Used to call functions after page is fully loaded.
+function main() {
+    //get request
+    const url = '/library/get'
+
+    renderFbc()
+}
+
+function renderFbc() {
+    const imgUrl = '/images/fbc.png'
+    for (let i = 0; i < fbcs.length; i++) {
+        const html = `
+        <div class="col-lg-3 col-md-6 col-sm-6 mb-4 post-column">
+            <a href="" id="${fbcs[i].id}">
+                <div class="card shadow postCard">
+                    <div class="image-constrain">
+                        <img src="${imgUrl}" class="card-img-top post-img p-3">
+                    </div>
+                    <div class="card-body text-center">
+                        <h6 class="post-title font-weight-bold font-italic text-truncate">FBC 1</h6>
+                    </div>
+                </div>
+            </a>
+        </div>`
+        const rowNum = Math.ceil((i + 1) /4)
+        if (i % 4 == 0) {
+            $('#post-TwoRow').append(`<div id="row${rowNum}"class="row fadeIn post-row"></div>`)
+        }
+        $(`#row${rowNum}`).append(html)
     }
 }
 
-const user1 = new User("1");
-const user2 = new User("2");
-const user3 = new User("3");
-const user4 = new User("4");
-const user5 = new User("5");
-const user6 = new User("6");
-const user7 = new User("7");
 
-// Fbc 1
-const mockUsers1 = [];
-mockUsers1.push(user1);
-const fbc1 = new Fbc(mockUsers1, "fbc1");
-
-// Fbc 2
-const mockUsers2 = [];
-mockUsers2.push(user1);
-mockUsers2.push(user2);
-const fbc2 = new Fbc(mockUsers2, "fbc2");
-
-// Fbc 3
-const mockUsers3 = [];
-mockUsers3.push(user1);
-mockUsers3.push(user2);
-mockUsers3.push(user3);
-const fbc3 = new Fbc(mockUsers3, "fbc3");
-
-// Fbc 4
-const mockUsers4 = [];
-mockUsers4.push(user1);
-mockUsers4.push(user2);
-mockUsers4.push(user3);
-mockUsers4.push(user4);
-const fbc4 = new Fbc(mockUsers4, "fbc4");
-
-// Fbc 5
-const mockUsers5 = [];
-mockUsers5.push(user1);
-mockUsers5.push(user2);
-mockUsers5.push(user3);
-mockUsers5.push(user4);
-mockUsers5.push(user5);
-const fbc5 = new Fbc(mockUsers5, "fbc5");
-
-// Fbc 6
-const mockUsers6 = [];
-mockUsers6.push(user1);
-mockUsers6.push(user2);
-mockUsers6.push(user3);
-mockUsers6.push(user4);
-mockUsers6.push(user5);
-mockUsers6.push(user6);
-const fbc6 = new Fbc(mockUsers6, "fbc6");
-
-// Fbc 7
-const mockUsers7 = [];
-mockUsers7.push(user1);
-mockUsers7.push(user2);
-mockUsers7.push(user3);
-mockUsers7.push(user4);
-mockUsers7.push(user5);
-mockUsers7.push(user6);
-mockUsers7.push(user7);
-const fbc7 = new Fbc(mockUsers7, "fbc7");
-
-const fbcs = [];
-fbcs.push(fbc1);
-fbcs.push(fbc2);
-fbcs.push(fbc3);
-fbcs.push(fbc4);
-fbcs.push(fbc5);
-fbcs.push(fbc6);
-fbcs.push(fbc7);
 
 let manageUsersContent = ''
 manageUsersContent += '<div class="modal-body">\n'
@@ -121,6 +92,9 @@ for(let i = 0; i < fbcs.length; i++){
 }
 manageCanvasContent += '</form>\n'
 manageCanvasContent += '</div>'
+
+
+
 
 function manageUsers(){
     $(".modal-title").html("Manage users")
